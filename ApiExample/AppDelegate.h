c26a6155980359cdc89454c3c1f0c2c45686dbca
nalin <nalinchhajer@yahoo.com>
2016-05-18 09:03:07 +0530

    Initial Commit

---
+++ ApiExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ApiExample
+//
+//  Created by Nalin Chhajer on 18/05/16.
+//  Copyright © 2016 Indix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ApiExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ApiExample
+//
+//  Created by Nalin Chhajer on 18/05/16.
+//  Copyright © 2016 Indix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
